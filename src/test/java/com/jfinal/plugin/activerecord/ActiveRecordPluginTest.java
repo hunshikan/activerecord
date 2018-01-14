d3aff006e992a6af05bd6a23bb8f4ae884953f36
iaceob <iaceob@gmail.com>
2018-01-14 11:56:54 +0800

    move to plugin.

---
+++ src/test/java/com/jfinal/plugin/activerecord/ActiveRecordPluginTest.java
@@ -0,0 +1,58 @@
+package com.jfinal.plugin.activerecord;
+
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
+import com.jfinal.plugin.druid.DruidPlugin;
+import io.enoa.json.kit.JsonKit;
+import io.enoa.toolkit.sys.PathKit;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ActiveRecordPluginTest {
+
+  @Before
+  public void before() {
+    DruidPlugin dp = new DruidPlugin(
+      "jdbc:mysql://localhost:3306/enoa?useUnicode=true&characterEncoding=utf-8&useSSL=false&nullNamePatternMatchesAll=true",
+      "root",
+      "passwd"
+    );
+    dp.setInitialSize(3).setMaxActive(10);
+    dp.addFilter(new StatFilter());
+
+    WallFilter wall = new WallFilter();
+    wall.setDbType("mysql");
+    WallConfig wc = new WallConfig();
+    wc.setMultiStatementAllow(true);
+    wc.setCommentAllow(true);
+
+    wall.setConfig(wc);
+    dp.addFilter(wall);
+
+    if (!dp.start())
+      throw new RuntimeException("Can not start activerecord plugin");
+
+    ActiveRecordPlugin arp = new ActiveRecordPlugin(dp.getDataSource());
+    arp.setShowSql(true);
+    arp.setDialect(new MysqlDialect());
+
+    String sqlBasePath = PathKit.getRootClassPath();
+
+    arp.setBaseSqlTemplatePath(sqlBasePath);
+    arp.addSqlTemplate("template.sql");
+
+    if (!arp.start())
+      throw new RuntimeException("Can not start activerecord plugin");
+  }
+
+  @Test
+  public void testActiveRecord() {
+    List<Record> records = Db.find(Db.getSql("User.list"));
+    System.out.println(JsonKit.toJson(records));
+  }
+
+}
